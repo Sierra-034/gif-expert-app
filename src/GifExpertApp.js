0406a7e022aaad1cdb37ce7fce1861d820f394b6
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-01-04 22:33:00 -0600

    Comunicación entre componentes
    Componente hijo que modifica el estado de
    su componente padre mediante setState.

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -5,16 +5,11 @@ const GifExpertApp = () => {
 
     const [ categories, setCategories ] = useState(['One Punch', 'Samurai X', 'Dragon ball']);
 
-    // const handleAdd = () => {
-    //     setCategories( prevState => [ ...prevState, 'Scooby Doo' ] );
-    // };
-
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory />
+            <AddCategory setCategories={ setCategories } />
             <hr/>
-            {/* <button onClick={ handleAdd }>Agregar</button> */}
             <ol>
                 { categories.map(category => {
                     return <li key={ category }>{ category }</li>
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import PropTypes from "prop-types";
 
-export const AddCategory = () => {
-    const [inputValue, setInputValue] = useState('Hola mundo');
+export const AddCategory = ({ setCategories }) => {
+    
+    const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = ( event ) => {
         setInputValue( event.target.value );
@@ -9,12 +11,14 @@ export const AddCategory = () => {
 
     const handleSubmit = ( event ) => {
         event.preventDefault();
-        console.log('Submit hecho');
+        if( inputValue.trim().length > 2 ) {
+            setCategories( categories => [...categories, inputValue] );
+            setInputValue('');
+        }
     };
     
     return (
         <form onSubmit={ handleSubmit } >
-            <h1>{ inputValue }</h1>
             <input
                 type="text"
                 value={ inputValue }
@@ -22,4 +26,8 @@ export const AddCategory = () => {
             />
         </form>
     );
+};
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
 };
