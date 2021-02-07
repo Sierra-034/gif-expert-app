a91c7ccaca4be2b38ab8cc9b9c9669402b6bd3ed
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-02-07 04:50:13 -0600

    Custom Hook

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -4,7 +4,7 @@ import { GifGrid } from "./components/GifGrid";
 
 const GifExpertApp = () => {
 
-    const [ categories, setCategories ] = useState([]);
+    const [ categories, setCategories ] = useState(["Halo"]);
 
     return (
         <>
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { GifGridItem } from "./GifGridItem";
-import { getGifs } from "../helpers/getGifs";
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+// import { GifGridItem } from "./GifGridItem";
+// import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
     
-    useEffect(() => {
-        getGifs(category)
-            .then(setImages);
-    }, [category]);
+    // useEffect(() => {
+    //     getGifs(category)
+    //         .then(setImages);
+    // }, [category]);
+
+    const {loading} = useFetchGifs();
+    console.log(loading);
 
     return (
         <>
         <h3>{ category }</h3>
-        <div className="card-grid">
+        {loading ? "Cargando..." : "Data cargada"}
+        {/* <div className="card-grid">
             {images.map(img => (
                 <GifGridItem
                     key={img.id}
                     {...img}
                 />
             ))}
-        </div>
+        </div> */}
         </>
     );
 };
