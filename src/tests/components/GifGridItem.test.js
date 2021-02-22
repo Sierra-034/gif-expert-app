58afbfb2f17499d8cad6f8997f84e708044d6712
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-02-21 23:58:34 -0600

    90: Pruebas del componentes - GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -2,8 +2,29 @@ import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe('Pruebas sobre <GifGridItem />', () => {
+
+    const title = 'Un título';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+    
     test('Muestra el componente correctamente', () => {
-        const wrapper = shallow(<GifGridItem />);
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Tener un párrafo con el título', () => {
+        const p = wrapper.find('p');
+        expect(p.text()).toBe(title);
+    });
+
+    test('Tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('Tiene la clase css animate__fadeIn', () => {
+        const expectedCssClass = 'animate__fadeIn';
+        const div = wrapper.find('div');
+        expect(div.hasClass(expectedCssClass)).toBe(true);
     });
 });
