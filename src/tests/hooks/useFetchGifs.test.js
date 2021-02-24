dee0fe1a80c5620c69dd96294ee18c590de48b64
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-02-23 22:13:13 -0600

    100: customHook - waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,11 +2,22 @@ import { renderHook } from '@testing-library/react-hooks';
 import { useFetchGifs } from '../../hooks/useFetchGifs';
 
 describe('Custom hook useFetchGifs', () => {
-    test('Verifica el estado inicial', () => {
-        const {result} = renderHook(() => useFetchGifs('Halo'));
+    test('Verifica el estado inicial', async () => {
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Halo'));
         const {data, loading} = result.current;
 
+        await waitForNextUpdate();
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+    });
+
+    test('Retorna un arreglo de imgs y loading en false', async () => {
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Halo'));
+        await waitForNextUpdate();
+
+        const {data, loading} = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
     });
 });
